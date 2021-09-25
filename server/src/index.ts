e4678a64482c1305edf1edec17034749772333e9
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2021-09-25 15:52:08 -0700

    introduce Apollo Server with a simple GraphQL schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,21 @@
 import express from 'express';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.use(express.json());
-
-app.get("/listings", (_req, res) => {
-	return res.send(listings);
+const server = new ApolloServer({
+  schema
 });
 
-app.post("/delete-listing", (req, res) => {
-	const id: string = req.body.id;
-
-	for (let i = 0; i < listings.length; i++) {
-		if (listings[i].id == id) {
-			return res.send(listings.splice(i, 1));
-		}
-	}
-
-	return res.send("Failed to delete listing.");
-})
-
-app.listen(port);
-
-console.log(`[app]: http://localhost:${port}`);
+(async () => {
+  await server.start().then(() => {
+    server.applyMiddleware({
+      app,
+      path: '/api'
+    });
+    app.listen(port);
+    console.log(`[app]: http://localhost:${port}`);
+  });
+})();
